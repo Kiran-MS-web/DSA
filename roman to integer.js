3a249657997e6d3c9b538e6295dff5483411974a
Kiran-MS-web <kiranms880@gmail.com>
2025-08-07 21:19:40 +0530

    added

---
+++ roman to integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+   let romanMap ={
+        I   :  1,
+        V  : 5,
+        X   :   10,
+        L   : 50,
+        C    : 100,
+        D    : 500,
+        M :  1000
+   } ;
+   let total = 0;
+   let i=0;
+   while (i<s.length){
+    let cv = romanMap[s[i]];
+    let nv = romanMap[s[i+1]];
+    if(nv>cv){
+        total += (nv-cv);
+        i += 2;
+    }else{
+        total += cv;
+        i += 1;
+    }
+    
+   }
+  return total;
+};
---
+++ valid parentheses.js
@@ -0,0 +1,18 @@
+function isValid(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '[' || ch === '{') {
+            stack.push(ch);
+        } else {
+            if (stack.pop() !== map[ch]) return false;
+        }
+    }
+
+    return stack.length === 0;
+}
