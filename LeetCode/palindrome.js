43e49f3b31df928e1a355f9ee2bc43ec25a07ed4
Kiran-MS-web <kiranms880@gmail.com>
2025-08-14 20:29:17 +0530

    9th questiopn

--- LeetCode/palindrome.js
+++ LeetCode/palindrome.js
@@ -12,3 +12,21 @@ var isPalindrome = function(x) {
     return reverse == x;
 };
 console.log(isPalindrome(121)); // true
+
+
+//of String
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;    
+};
